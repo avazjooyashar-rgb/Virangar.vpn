16103c82eebdba63541533ae44be70b93dc503c1
avazjooyashar-rgb <avazjooyashar@gmail.com>
2026-09-23 19:31:09 +0330

    Create user_admin_menu.py

---
+++ user_admin_menu.py
@@ -0,0 +1,13 @@
+from config import bot
+from decorators import super_admin_only
+from keyboards import admin_keyboard
+
+
+@bot.message_handler(func=lambda m: m.text == "👑 پنل سوپر ادمین")
+@super_admin_only
+def user_switch_to_admin_menu(message):
+    bot.send_message(
+        message.chat.id,
+        "👑 وارد پنل سوپر ادمین شدید.",
+        reply_markup=admin_keyboard()
+    )
