2eb2ee032e625b2cc1ca3ebf96b9583e26deab04
avazjooyashar-rgb <avazjooyashar@gmail.com>
2026-09-23 19:36:48 +0330

    Update admin_user_menu.py

--- admin_user_menu.py
+++ admin_user_menu.py
@@ -1,18 +1,21 @@
 # ============================================================
-# user_admin_menu.py
-# سوییچ مستقیم سوپر ادمین از منوی کاربر به منوی ادمین
+# admin_user_menu.py
+# سوییچ مستقیم ادمین به منوی کاربر (بدون اجرای دوباره‌ی /start)
 # ============================================================
 
 from config import bot
-from decorators import super_admin_only
-from keyboards import admin_keyboard
+from decorators import admin_only
+from keyboards import user_keyboard
+from models import is_superadmin
 
 
-@bot.message_handler(func=lambda m: m.text == "👑 پنل سوپر ادمین")
-@super_admin_only
-def user_switch_to_admin_menu(message):
+@bot.message_handler(func=lambda m: m.text == "🏠 منوی کاربر")
+@admin_only
+def admin_switch_to_user_menu(message):
     bot.send_message(
         message.chat.id,
-        "👑 وارد پنل سوپر ادمین شدید.",
-        reply_markup=admin_keyboard()
+        "🏠 وارد منوی کاربر شدید.",
+        reply_markup=user_keyboard(
+            is_super_admin=is_superadmin(message.from_user.id)
+        )
     )
