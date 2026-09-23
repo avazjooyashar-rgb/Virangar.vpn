4984b4f4f036194b63523cf37d4ab1d0b09eee5a
avazjooyashar-rgb <avazjooyashar@gmail.com>
2026-09-23 18:40:24 +0330

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -6,13 +6,27 @@
 from functools import wraps
 
 from config import bot
-from models import is_admin
+from models import is_admin, is_superadmin
 
 
 def admin_only(func):
     @wraps(func)
     def wrapper(message, *args, **kwargs):
         if not is_admin(message.from_user.id):
+            bot.send_message(
+                message.chat.id,
+                "⛔ دسترسی غیرمجاز"
+            )
+            return
+        return func(message, *args, **kwargs)
+
+    return wrapper
+
+
+def super_admin_only(func):
+    @wraps(func)
+    def wrapper(message, *args, **kwargs):
+        if not is_superadmin(message.from_user.id):
             bot.send_message(
                 message.chat.id,
                 "⛔ دسترسی غیرمجاز"
